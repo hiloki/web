0474c66bb8354761ace2e2a1aa5bba4d794c2620
t32k <ijok.ijok@gmail.com>
2014-04-02 01:08:01 +0900

    WIP

--- app.js
+++ app.js
@@ -10,10 +10,53 @@ var http = require('http'),
 
 var app = express();
 
+var moment = require('moment');
+
+var _ = require('underscore');
+_.str = require('underscore.string');
+_.mixin(_.str.exports());
+_.str.include('Underscore.string', 'string');
+var numeral = require('numeral');
+
+/**
+ * Prettify StyleStats data.
+ * @param {object} [result] StyleStats parse data. Required.
+ * @return {array} prettified data.
+ */
+function prettify(result) {
+    var collections = [];
+    Object.keys(result).forEach(function(key) {
+        var stats = {};
+        stats.name = key;
+        stats.prop = _(_(key).humanize()).titleize();
+        if (key === 'propertiesCount') {
+            var array = [];
+            result[key].forEach(function(item) {
+                array.push([item.property, item.count]);
+            });
+            stats.value = array.join('<br>').replace(/\,/g, ': ');
+        } else if (key === 'published') {
+            stats.value = moment().format('llll');
+        } else if (key === 'size' || key === 'gzippedSize' || key === 'dataUriSize') {
+            stats.value = numeral(result[key]).format('0.0b').replace(/\.0B/, 'B').replace(/0\.0/, '0');
+        } else if (key === 'simplicity' || key === 'ratioOfDataUriSize') {
+            stats.value = numeral(result[key]).format('0.00%');
+        } else {
+            stats.value = Array.isArray(result[key]) ? result[key].join('<br>') : result[key];
+            if (stats.value === '') {
+                stats.value = 'N/A';
+            }
+        }
+        collections.push(stats);
+    });
+    return collections;
+}
+
 // all environments
 app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.locals.pretty = true;
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -28,10 +71,11 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
+
 app.post('/parse', function(req, res) {
     var stats = new StyleStats(req.body.path);
     stats.parse(function(result) {
-        res.send(result);
+        res.send(prettify(result));
     });
 });
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,83 @@
-$('#js-parse').on('click', function() {
-    var $that = $(this);
-    $that.text('').addClass('is-loading');
-    var url = $('#js-url').val();
-    $.ajax({
-        type: 'POST',
-        url: '/parse',
-        data: {
-            path: url
+(function() {
+
+    $(document).ready(function() {
+        console.log();
+        if (location.hash === "#upload") {
+            $('#js-tabs > li').each(function() {
+                $(this).removeClass('is-active');
+            });
+            $('[href="#upload"]').parent().addClass('is-active');
+            $('#js-tabContents > li').each(function() {
+                $(this).removeClass('is-active');
+            });
+            $('#js-upload').parent().addClass('is-active');
         }
-    }).done(function(result) {
-        $that.text('Parse').removeClass('is-loading');
-        alert('Loaded!');
-        console.log(result);
+        if (location.hash === "#input") {
+            $('#js-tabs > li').each(function() {
+                $(this).removeClass('is-active');
+            });
+            $('[href="#input"]').parent().addClass('is-active');
+            $('#js-tabContents > li').each(function() {
+                $(this).removeClass('is-active');
+            });
+            $('#js-input').parent().addClass('is-active');
+        }
     });
-});
+    // Tab Change
+    $('#js-tabs > li').on('click', function() {
+        $('#js-tabs > li').each(function() {
+            $(this).removeClass('is-active');
+        });
+        $(this).addClass('is-active');
+
+        $('#js-tabContents > li').each(function() {
+            $(this).removeClass('is-active');
+        });
+        var content = '#js-' + $(this).data('tabs');
+        $(content).parent().addClass('is-active');
+    });
+
+
+    var tmpl = $('#js-tmpResult').html();
+    var compiled = _.template(tmpl);
+
+    $('#js-parse').on('click', function() {
+        var $that = $(this);
+
+        var mode = '';
+        $('#js-tabs > li').each(function() {
+            if ($(this).hasClass('is-active')) {
+                mode = $(this).data('tabs');
+            }
+        });
+
+        var selector = '#js-' + mode;
+        if ($(selector).val()) {
+            var paths = $(selector).val();
+        } else {
+            window.alert('No Input.')
+            return false;
+        }
+
+
+        $that.text('').addClass('is-loading');
+
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/parse',
+            data: {
+                path: paths
+            }
+        }).done(function(data) {
+            $that.text('Parse').removeClass('is-loading');
+
+            $('#js-view').html(compiled({
+                results: data
+            }));
+
+        });
+    });
+
+}());
