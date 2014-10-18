56821c45fafa3445dfe95b519adfe08bb2af4a49
1000ch <shogo.sensui@gmail.com>
2014-10-18 16:39:08 +0900

    refactoring

---
+++ routes/parse.js
@@ -0,0 +1,14 @@
+var StyleStats = require('stylestats');
+var prettify = require('stylestats/lib/prettify');
+
+module.exports = function(request, response) {
+  var path = request.body.path;
+  var css = request.body.css;
+  var stylestats = new StyleStats(path || css);
+  stylestats.parse(function(error, result) {
+    if(error) {
+      response.send(500, 'Something broken!');
+    }
+    response.send(prettify(result));
+  });
+};
