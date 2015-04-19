9f271269277fed8c16fa8434df4a0a160a2ef8bb
t32k <ijok.ijok@gmail.com>
2015-04-18 22:10:39 -0700

    Replace grunt-csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   gulp.src('stylus/app.styl')
     .pipe(stylus())
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
+    .pipe(gulp.dest('public/css/'))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('public/css/'));
 });
