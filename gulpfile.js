c5a89dc3d9ffccb6cefb10c45457fc7025ba7c45
t32k <ijok.ijok@gmail.com>
2015-04-18 22:40:55 -0700

    Goodbye grunt

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,24 @@
 var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var autoprefixer = require('gulp-autoprefixer');
-var cssmin = require('gulp-cssmin');
-var csscomb = require('gulp-csscomb');
-var rename = require('gulp-rename');
-var browserify = require('gulp-browserify');
-var uglify = require('gulp-uglify');
+var $ = require('gulp-load-plugins')();
 
 gulp.task('css', function () {
   gulp.src('stylus/app.styl')
-    .pipe(stylus())
-    .pipe(autoprefixer({
+    .pipe($.stylus())
+    .pipe($.autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(csscomb())
+    .pipe($.csscomb())
     .pipe(gulp.dest('public/css/'))
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
+    .pipe($.cssmin())
+    .pipe($.rename({suffix: '.min'}))
     .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('js', function () {
   gulp.src('public/js/app.js')
-    .pipe(browserify())
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
+    .pipe($.browserify())
+    .pipe($.uglify())
+    .pipe($.rename({suffix: '.min'}))
     .pipe(gulp.dest('public/js/'));
 });
 
