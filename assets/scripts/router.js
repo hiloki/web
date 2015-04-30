cd1b79a30e0cfb8985f13b0ac8e4c3010840a748
t32k <ijok.ijok@gmail.com>
2015-04-30 13:05:26 -0700

    Delete client routes

--- assets/scripts/router.js
+++ assets/scripts/router.js
@@ -1,11 +1,5 @@
-var order = require('stylestats/assets/aliases.json');
-
 module.exports = Parse.Router.extend({
   routes: {
     'results/:id': 'results'
-  },
-  results: function (id) {
-    var rawData = JSON.parse($('#js-result').text());
-    global.result.set(rawData);
   }
 });
--- routes/results.js
+++ routes/results.js
@@ -1,20 +1,41 @@
+var hbs = require('express-handlebars').create();
+var prettify = require('stylestats/lib/prettify.js');
+
+function processData(data) {
+  var KEY_ARRY = [
+    'Paths',
+    'Lowest Cohesion Selector',
+    'Unique Font Families',
+    'Unique Font Sizes',
+    'Unique Colors',
+    'Properties Count'
+  ];
+  Object.keys(data).forEach(function (key) {
+    if (KEY_ARRY.indexOf(key) !== -1) {
+      data[key] = data[key].replace(/\n/g, '<br>').split('<br>');
+    }
+  });
+}
+
+var Parse = require('parse').Parse;
+var Result = Parse.Object.extend('Result');
+var query = new Parse.Query(Result);
+
 module.exports = function (request, response) {
-
   var id = request.params[0];
-  var Parse = require('parse').Parse;
-  var Result = Parse.Object.extend('Result');
-  var query = new Parse.Query(Result);
-
   query.equalTo('objectId', id);
   query.first().then(function (data) {
     if (data) {
+      var result = prettify(data.attributes);
+      processData(result);
       var title = data.get('paths')[0] + ' - ' + data.createdAt;
-
-      var result = JSON.stringify(data.attributes);
-      response.render('index', {
-        title: 'StyleStats Test Result | ' + title,
-        result: result
-      });
+      hbs.render('./assets/template/perfect-list.hbs', {data: result})
+        .then(function (html) {
+          response.render('index', {
+            title: 'StyleStats Test Result | ' + title,
+            result: html
+          });
+        });
     } else {
       response.render('404', {
         title: "Test not found | StyleStats",
