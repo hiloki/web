b9d5ea8e08f4528bc0b357487b6561b31eb9e561
t32k <ijok.ijok@gmail.com>
2015-05-03 01:01:13 -0700

    Change save prperty ranki limit

--- routes/parse.js
+++ routes/parse.js
@@ -1,8 +1,11 @@
 var StyleStats = require('stylestats');
+var config = {
+  "propertiesCount": 1000
+};
 
 module.exports = function (request, response) {
   var path = request.body.path;
-  var stylestats = new StyleStats(path);
+  var stylestats = new StyleStats(path, config);
   stylestats.parse(function (error, result) {
     if (error) {
       response.send(500, 'Something broken!');
